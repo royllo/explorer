2d48b03e4cf63f015ce5c20964b7835554314850
straumat <stephane.traumat@gmail.com>
2022-10-21 17:49:10 +0200

    Backend - processAddAssetRequest() without proof management

--- backend/explorer-batch/src/main/java/org/royllo/explorer/batch/service/RequestProcessorServiceImplementation.java
+++ backend/explorer-batch/src/main/java/org/royllo/explorer/batch/service/RequestProcessorServiceImplementation.java
@@ -3,13 +3,17 @@ package org.royllo.explorer.batch.service;
 import lombok.RequiredArgsConstructor;
 import org.royllo.explorer.batch.util.BaseProcessor;
 import org.royllo.explorer.core.domain.bitcoin.BitcoinTransactionOutput;
+import org.royllo.explorer.core.dto.asset.AssetDTO;
 import org.royllo.explorer.core.dto.request.AddAssetRequestDTO;
 import org.royllo.explorer.core.dto.request.RequestDTO;
 import org.royllo.explorer.core.repository.bitcoin.BitcoinTransactionOutputRepository;
 import org.royllo.explorer.core.repository.request.RequestRepository;
+import org.royllo.explorer.core.service.asset.AssetService;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
+import static org.royllo.explorer.core.util.constants.UserConstants.ANONYMOUS_USER;
 
 /**
  * Request processor service implementation.
@@ -25,9 +29,15 @@ public class RequestProcessorServiceImplementation extends BaseProcessor impleme
     /** Request repository. */
     private final RequestRepository requestRepository;
 
+    /** Asset service. */
+    private final AssetService assetService;
+
     @Override
     public RequestDTO processRequest(final RequestDTO requestDTO) {
+        assert !requestDTO.getStatus().isFinalStatus() : "This request has already been treated";
+
         // Add asset request.
+        // TODO Simplify when pattern Matching for switch will be available.
         if (requestDTO.getClass().equals(AddAssetRequestDTO.class)) {
             return processAddAssetRequest((AddAssetRequestDTO) requestDTO);
         }
@@ -46,18 +56,35 @@ public class RequestProcessorServiceImplementation extends BaseProcessor impleme
         final Optional<BitcoinTransactionOutput> transactionOutput = bitcoinTransactionOutputRepository.findByTxIdAndVout(addAssetRequestDTO.getTxId(), addAssetRequestDTO.getVout());
         if (transactionOutput.isEmpty()) {
             // Transaction not found in database.
-            addAssetRequestDTO.setErrorMessage("Transaction not found (" + addAssetRequestDTO.getGenesisPoint() + ")");
+            addAssetRequestDTO.failed("Transaction not found (" + addAssetRequestDTO.getGenesisPoint() + ")");
             requestRepository.save(REQUEST_MAPPER.mapToAddAssetRequest(addAssetRequestDTO));
             return addAssetRequestDTO;
         }
 
         // =============================================================================================================
         // We check the provided proof.
+        if (!"VALID_PROOF".equals(addAssetRequestDTO.getProof())) {
+            addAssetRequestDTO.failed("Invalid proof");
+            requestRepository.save(REQUEST_MAPPER.mapToAddAssetRequest(addAssetRequestDTO));
+            return addAssetRequestDTO;
+        }
 
         // =============================================================================================================
-        // Everything is ok, we now create the asset.
-
-        return null;
+        // Everything is ok, we now create the asset, update the request and return it.
+        final AssetDTO assetCreated = assetService.addAsset(
+                AssetDTO.builder()
+                        .genesisPoint(BITCOIN_MAPPER.mapToBitcoinTransactionOutputDTO(transactionOutput.get()))
+                        .creator(ANONYMOUS_USER)
+                        .name(addAssetRequestDTO.getName())
+                        .metaData(addAssetRequestDTO.getMetaData())
+                        .assetId(addAssetRequestDTO.getAssetId())
+                        .outputIndex(addAssetRequestDTO.getOutputIndex())
+                        .build()
+        );
+        addAssetRequestDTO.setAsset(assetCreated);
+        addAssetRequestDTO.succeed();
+        requestRepository.save(REQUEST_MAPPER.mapToAddAssetRequest(addAssetRequestDTO));
+        return addAssetRequestDTO;
     }
 
 }
