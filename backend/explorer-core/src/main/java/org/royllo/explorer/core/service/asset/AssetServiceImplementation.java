acf66437fec63f9da11d82f6eeee31010bec8df0
straumat <stephane.traumat@gmail.com>
2022-10-23 23:12:46 +0200

    Added logs on the service layer

--- backend/explorer-core/src/main/java/org/royllo/explorer/core/service/asset/AssetServiceImplementation.java
+++ backend/explorer-core/src/main/java/org/royllo/explorer/core/service/asset/AssetServiceImplementation.java
@@ -9,8 +9,11 @@ import org.royllo.explorer.core.service.bitcoin.BitcoinService;
 import org.royllo.explorer.core.util.base.BaseService;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.royllo.explorer.core.util.constants.UserConstants.ANONYMOUS_USER;
 
@@ -30,23 +33,39 @@ public class AssetServiceImplementation extends BaseService implements AssetServ
 
     @Override
     public List<AssetDTO> queryAssets(final String value) {
+        List<AssetDTO> results = new LinkedList<>();
+
         final Optional<Asset> firstSearch = assetRepository.findByAssetId(value);
         if (firstSearch.isPresent()) {
             // We found an asset with the corresponding assetId, we return it.
-            return firstSearch.stream()
+            results = firstSearch.stream()
                     .map(ASSET_MAPPER::mapToAssetDTO)
                     .toList();
         } else {
             // We search all assets where "value" is in the name.
-            return assetRepository.findByNameContainsIgnoreCase(value)
+            results = assetRepository.findByNameContainsIgnoreCase(value)
                     .stream()
                     .map(ASSET_MAPPER::mapToAssetDTO)
                     .toList();
         }
+
+        // Creating logs.
+        if (results.isEmpty()) {
+            logger.info("queryAssets - For '{}', there is no results", value);
+        } else {
+            logger.info("queryAssets - For '{}', {} result(s) with assets id(s): {}", value,
+                    results.size(),
+                    results.stream()
+                            .map(AssetDTO::getId)
+                            .map(Objects::toString)
+                            .collect(Collectors.joining(", ")));
+        }
+        return results;
     }
 
     @Override
     public AssetDTO addAsset(final AssetDTO newAsset) {
+        logger.info("addAsset - Adding {}", newAsset);
         // We check constraints.
         assert newAsset.getId() == null : "Asset already exists";
         assert newAsset.getGenesisPoint() != null : "Bitcoin transaction is required";
@@ -71,12 +90,26 @@ public class AssetServiceImplementation extends BaseService implements AssetServ
 
     @Override
     public Optional<AssetDTO> getAsset(final long id) {
-        return assetRepository.findById(id).map(ASSET_MAPPER::mapToAssetDTO);
+        final Optional<Asset> asset = assetRepository.findById(id);
+        if (asset.isEmpty()) {
+            logger.info("getAsset - Asset with id {} not found", id);
+            return Optional.empty();
+        } else {
+            logger.info("getAsset - Asset with id {} found: {}", id, asset.get());
+            return asset.map(ASSET_MAPPER::mapToAssetDTO);
+        }
     }
 
     @Override
     public Optional<AssetDTO> getAssetByAssetId(final String assetId) {
-        return assetRepository.findByAssetId(assetId).map(ASSET_MAPPER::mapToAssetDTO);
+        final Optional<Asset> asset = assetRepository.findByAssetId(assetId);
+        if (asset.isEmpty()) {
+            logger.info("getAssetByAssetId - Asset with assetId {} not found", assetId);
+            return Optional.empty();
+        } else {
+            logger.info("getAssetByAssetId - Asset with assetId {} found: {}", assetId, asset.get());
+            return asset.map(ASSET_MAPPER::mapToAssetDTO);
+        }
     }
 
 }
