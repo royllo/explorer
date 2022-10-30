db5c5932f41c22428e7a24f8a5d4d76b494f5154
straumat <stephane.traumat@gmail.com>
2022-10-30 19:11:07 +0100

    Added footer

--- clients/web/components/defaultHeader.vue
+++ clients/web/components/defaultHeader.vue
@@ -1,10 +1,9 @@
 <template>
   <!-- ============================================================================================================= -->
-  <!-- Our header only has a button to register an asset -->
+  <!-- Our header only has a button to register an asset                                                             -->
   <!-- ============================================================================================================= -->
-
   <!--
-        Header.
+        CSS explanation:
           - flex: The Flexbox module aims at providing a more efficient way to lay out, align and distribute space among
           items in a container, even when their size is unknown and/or dynamic.
           - items-center to align items along the center of the container’s cross axis (useless when we have only one).
@@ -13,11 +12,15 @@
           - px-{size} to add horizontal padding.
           - py-{size} to add vertical padding.
           - bg-{color} to set the background color.
+          - .rounded-sm, .rounded, or .rounded-lg to apply different border radius sizes to an element.
   -->
+
   <header class="flex items-center justify-end px-4 py-4 bg-white">
-    <button
-        class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-    >Add your asset
+
+    <button class="px-4 py-2 font-bold text-white rounded bg-blue-500 hover:bg-blue-700">
+      Add your asset
     </button>
+
   </header>
+
 </template>
--- clients/web/layouts/default.vue
+++ clients/web/layouts/default.vue
@@ -1,12 +1,8 @@
-<script setup>
-// Current year used to write the copyright year.
-const currentYear = new Date().getFullYear();
-</script>
-
 <template>
   <div class="flex flex-col h-screen justify-between">
 
-    <DefaultHeader/>
+    <!-- Our header -->
+    <defaultHeader/>
 
     <!--
         ================================================================================================================
@@ -20,34 +16,8 @@ const currentYear = new Date().getFullYear();
       <slot/>
     </main>
 
-    <!--
-        ================================================================================================================
-        Footer
-        ================================================================================================================
-    -->
-    <footer class="p-4 bg-gray-100 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-      <!-- Left part : Royllo copyright and link -->
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-      © {{ currentYear }} <a href="https://www.royllo.org"
-                             class="hover:underline"
-                             target="royllo_website">Royllo</a>. All Rights Reserved.
-      </span>
-      <!-- Right part : Links to know more about Royllo -->
-      <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-        </li>
-        <li>
-          <a href="https://github.com/royllo/explorer" class="mr-4 hover:underline md:mr-6" target="github">Github</a>
-        </li>
-        <li>
-          <a href="https://twitter.com/royllo_systems" class="mr-4 hover:underline md:mr-6" target="twitter">Twitter</a>
-        </li>
-        <li>
-          <a href="mailto:contact@royllo.org" class="hover:underline">Contact</a>
-        </li>
-      </ul>
-    </footer>
+    <!-- Our header -->
+    <defaultFooter/>
 
   </div>
 </template>
