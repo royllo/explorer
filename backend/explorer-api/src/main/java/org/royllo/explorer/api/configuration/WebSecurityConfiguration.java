66640753bb07bc46f2678b2a7df518098bdb33a0
straumat <stephane.traumat@gmail.com>
2022-12-31 01:01:26 +0100

    Allow access to graphiql

--- backend/explorer-api/src/main/java/org/royllo/explorer/api/configuration/WebSecurityConfiguration.java
+++ backend/explorer-api/src/main/java/org/royllo/explorer/api/configuration/WebSecurityConfiguration.java
@@ -1,24 +1,30 @@
 package org.royllo.explorer.api.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebSecurity
 @SuppressWarnings("checkstyle:DesignForExtension")
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
+
     @Override
     protected final void configure(final HttpSecurity http) throws Exception {
-        http.antMatcher("/graphql/**")
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(STATELESS)
-                .and()
-                .authorizeRequests().anyRequest().permitAll();
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/graphql").permitAll()
+                .antMatchers("/graphiql").permitAll()
+                .antMatchers("/vendor/**").permitAll()
+                .anyRequest().authenticated();
     }
 
 }
