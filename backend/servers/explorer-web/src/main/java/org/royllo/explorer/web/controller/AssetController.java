8379e9fbaefb189a06475f43b7bdd03377f8a978
straumat <stephane.traumat@gmail.com>
2023-11-22 15:08:06 +0100

    Asset proofs done

--- backend/servers/explorer-web/src/main/java/org/royllo/explorer/web/controller/AssetController.java
+++ backend/servers/explorer-web/src/main/java/org/royllo/explorer/web/controller/AssetController.java
@@ -1,16 +1,13 @@
 package org.royllo.explorer.web.controller;
 
-import io.micrometer.common.util.StringUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.royllo.explorer.core.dto.asset.AssetDTO;
-import org.royllo.explorer.core.dto.asset.AssetStateDTO;
 import org.royllo.explorer.core.dto.proof.ProofDTO;
 import org.royllo.explorer.core.service.asset.AssetService;
 import org.royllo.explorer.core.service.asset.AssetStateService;
 import org.royllo.explorer.core.service.proof.ProofService;
 import org.royllo.explorer.web.util.base.BaseController;
-import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,9 +31,8 @@ import static org.royllo.explorer.web.util.constants.ModelAttributeConstants.ASS
 import static org.royllo.explorer.web.util.constants.ModelAttributeConstants.ASSET_ATTRIBUTE;
 import static org.royllo.explorer.web.util.constants.ModelAttributeConstants.ASSET_ID_ATTRIBUTE;
 import static org.royllo.explorer.web.util.constants.ModelAttributeConstants.ASSET_STATES_LIST_ATTRIBUTE;
-import static org.royllo.explorer.web.util.constants.ModelAttributeConstants.PROOF_FILES_LIST_ATTRIBUTE;
-import static org.royllo.explorer.web.util.constants.ModelAttributeConstants.PROOF_FILE_ID_ATTRIBUTE;
-import static org.royllo.explorer.web.util.constants.ModelAttributeConstants.RESULT_ATTRIBUTE;
+import static org.royllo.explorer.web.util.constants.ModelAttributeConstants.PROOF_ID_ATTRIBUTE;
+import static org.royllo.explorer.web.util.constants.ModelAttributeConstants.PROOF_LIST_ATTRIBUTE;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 /**
@@ -178,67 +174,32 @@ public class AssetController extends BaseController {
                               @PathVariable(value = ASSET_ID_ATTRIBUTE, required = false) final String assetId) {
         addAssetToModel(model, assetId);
 
+        // We retrieve the proof files.
+        model.addAttribute(PROOF_LIST_ATTRIBUTE,
+                proofService.getProofByAssetId(assetId.trim(),
+                        1,
+                        ASSET_PROOFS_DEFAULT_PAGE_SIZE));
+
         return getPageOrFragment(request, ASSET_PROOFS_TAB);
     }
 
     /**
-     * Page displaying an asset.
+     * Download a proof.
      *
-     * @param model   model
-     * @param request request
      * @param assetId asset id
-     * @return view asset page
+     * @param proofId proof id
+     * @return proof
      */
-    @SuppressWarnings("SameReturnValue")
-    public String backup(final Model model,
-                         final HttpServletRequest request,
-                         @PathVariable(value = ASSET_ID_ATTRIBUTE, required = false) final String assetId) {
-        // If assetId is present, we retrieve it.
-        if (StringUtils.isNotBlank(assetId)) {
-            model.addAttribute(ASSET_ID_ATTRIBUTE, assetId);
-
-            // =========================================================================================================
-            // We retrieve the asset and the assert group.
-            final Optional<AssetDTO> asset = assetService.getAssetByAssetId(assetId.trim());
-            if (asset.isPresent()) {
-                model.addAttribute(RESULT_ATTRIBUTE, asset.get());
-
-                // =========================================================================================================
-                // We retrieve the asset states.
-                final Page<AssetStateDTO> assetStates = assetStateService.getAssetStatesByAssetId(assetId.trim(), 1,
-                        ASSET_STATES_DEFAULT_PAGE_SIZE);
-                model.addAttribute(ASSET_STATES_LIST_ATTRIBUTE, assetStates.getContent());
-
-                // =========================================================================================================
-                // We retrieve the proof files.
-                model.addAttribute(PROOF_FILES_LIST_ATTRIBUTE,
-                        proofService.getProofByAssetId(assetId.trim(),
-                                1,
-                                ASSET_PROOFS_DEFAULT_PAGE_SIZE));
-            }
-        }
-
-        // If it's an HTMX request, we return the fragment.
-//        if (isHtmxRequest(request)) {
-//            return ASSET_PAGE_FRAGMENT;
-//        }
-        return ASSET_PAGE;
-    }
-
-    /**
-     * Returns a proof.
-     *
-     * @param proofFileId proof file id
-     * @return proof file
-     */
-    @GetMapping(value = "/asset/{assetId}/proof_file/{proofFileId}",
+    @GetMapping(value = "/asset/{assetId}/proof/{proofId}",
             produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public @ResponseBody byte[] getProofFile(@PathVariable(value = PROOF_FILE_ID_ATTRIBUTE) final String proofFileId) {
-        final Optional<ProofDTO> proofFile = proofService.getProofByProofId(proofFileId);
+    public @ResponseBody byte[] getProof(@PathVariable(value = ASSET_ID_ATTRIBUTE, required = false) final String assetId,
+                                         @PathVariable(value = PROOF_ID_ATTRIBUTE) final String proofId) {
+        final Optional<ProofDTO> proofFile = proofService.getProofByProofId(proofId);
         if (proofFile.isPresent()) {
             return proofFile.get().getProof().getBytes();
         } else {
-            throw new ResponseStatusException(NOT_FOUND, "Proof file not found");
+            throw new ResponseStatusException(NOT_FOUND,
+                    "Proof not found on asset id:" + assetId + " and proof id:" + proofId);
         }
     }
 
