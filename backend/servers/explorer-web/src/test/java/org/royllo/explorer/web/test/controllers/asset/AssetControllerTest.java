9045eb95284b47ba6110042b9794c16699499baf
straumat <stephane.traumat@gmail.com>
2023-11-23 09:48:33 +0100

    fixing AssetControllerTest

--- backend/servers/explorer-web/src/test/java/org/royllo/explorer/web/test/controllers/asset/AssetControllerTest.java
+++ backend/servers/explorer-web/src/test/java/org/royllo/explorer/web/test/controllers/asset/AssetControllerTest.java
@@ -11,11 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Objects;
 
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.containsString;
@@ -32,8 +29,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class AssetControllerTest extends BaseTest {
 
-    // TODO review this test
-
     @Autowired
     AssetRepository assetRepository;
 
@@ -44,55 +39,57 @@ public class AssetControllerTest extends BaseTest {
     MockMvc mockMvc;
 
     @Autowired
-    Environment environment;
-
-    @Autowired
     MessageSource messageSource;
 
     @ParameterizedTest
     @MethodSource("headers")
     @DisplayName("Asset page without parameter")
     void assetPageWithoutParameter(final HttpHeaders headers) throws Exception {
+
         mockMvc.perform(get("/asset/").headers(headers))
                 .andExpect(status().isOk())
                 .andExpect(view().name(containsString(ASSET_PAGE)))
                 // Checking error message.
                 .andExpect(content().string(containsString(getMessage(messageSource, "asset.view.error.noAssetId"))))
-                .andExpect(content().string(not(containsString(Objects.requireNonNull(getMessage(messageSource, "asset.view.error.assetNotFound"))
-                        .replace("\"{0}\"", "&quot;" + ROYLLO_COIN_ASSET_ID + "&quot;")))));
+                .andExpect(content().string(not(containsString(getMessage(messageSource, "asset.view.error.assetNotFound")))));
+
     }
 
     @ParameterizedTest
     @MethodSource("headers")
     @DisplayName("Asset page without parameter and slash")
     void assetPageWithoutParameterAndSlash(final HttpHeaders headers) throws Exception {
+
         mockMvc.perform(get("/asset").headers(headers))
                 .andExpect(status().isOk())
                 .andExpect(view().name(containsString(ASSET_PAGE)))
                 // Checking error message.
-                .andExpect(content().string(containsString(environment.getProperty("asset.view.error.noAssetId"))))
-                .andExpect(content().string(not(containsString(Objects.requireNonNull(
-                                environment.getProperty("asset.view.error.assetNotFound"))
-                        .replace("\"{0}\"", "&quot;" + ROYLLO_COIN_ASSET_ID + "&quot;")))));
-    }
+                .andExpect(content().string(containsString(getMessage(messageSource, "asset.view.error.noAssetId"))))
+                .andExpect(content().string(not(containsString(getMessage(messageSource, "asset.view.error.assetNotFound")))));
 
-    @Test
-    @DisplayName("Download proof")
-    void downloadProof() throws Exception {
-        mockMvc.perform(get("/asset/" + ROYLLO_COIN_ASSET_ID + "/proof_file/" + ROYLLO_COIN_FROM_TEST.getDecodedProofRequest(0).getProofId()))
-                .andExpect(status().isOk());
     }
 
     @ParameterizedTest
     @MethodSource("headers")
     @DisplayName("Invalid asset id")
     void invalidAssetId(final HttpHeaders headers) throws Exception {
-        final String expectedMessage = Objects.requireNonNull(environment.getProperty("asset.view.error.assetNotFound")).replace("\"{0}\"", "&quot;NON_EXISTING_ASSET_ID&quot;");
+
         mockMvc.perform(get("/asset/NON_EXISTING_ASSET_ID").headers(headers))
                 .andExpect(status().isOk())
                 .andExpect(view().name(containsString(ASSET_PAGE)))
                 // Checking error message.
-                .andExpect(content().string(containsString(expectedMessage)));
+                .andExpect(content().string(not(containsString(getMessage(messageSource, "asset.view.error.noAssetId")))))
+                .andExpect(content().string(containsString(getMessage(messageSource, "asset.view.error.assetNotFound"))));
+
+    }
+
+    @Test
+    @DisplayName("Download proof")
+    void downloadProof() throws Exception {
+
+        mockMvc.perform(get("/asset/" + ROYLLO_COIN_ASSET_ID + "/proof/" + ROYLLO_COIN_FROM_TEST.getDecodedProofRequest(0).getProofId()))
+                .andExpect(status().isOk());
+
     }
 
 }
