1f26afb8e832a24c88a5966599ac3ef81a99005d
straumat <stephane.traumat@gmail.com>
2024-01-03 17:37:48 +0100

    Add a cache for assets

--- backend/servers/explorer-web/src/main/java/org/royllo/explorer/web/configuration/WebConfiguration.java
+++ backend/servers/explorer-web/src/main/java/org/royllo/explorer/web/configuration/WebConfiguration.java
@@ -2,14 +2,19 @@ package org.royllo.explorer.web.configuration;
 
 import lombok.RequiredArgsConstructor;
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.jetbrains.annotations.NotNull;
 import org.royllo.explorer.core.util.parameters.IncomingRateLimitsParameters;
 import org.royllo.explorer.web.util.interceptor.RateLimitInterceptor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static java.util.concurrent.TimeUnit.DAYS;
 
 /**
  * Web configuration.
@@ -17,6 +22,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebConfiguration implements WebMvcConfigurer {
+
 
     /** Assets search results default page size. */
     public static final int ASSET_SEARCH_DEFAULT_PAGE_SIZE = 10;
@@ -29,6 +35,9 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     /** Asset's proofs default page size. */
     public static final int ASSET_PROOFS_DEFAULT_PAGE_SIZE = 10;
+
+    /** Default cache duration. */
+    public static final CacheControl DEFAULT_CACHE_DURATION = CacheControl.maxAge(1, DAYS);
 
     /** Incoming rate limits parameters. */
     private final IncomingRateLimitsParameters incomingRateLimitsParameters;
@@ -66,6 +75,32 @@ public class WebConfiguration implements WebMvcConfigurer {
         );
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Override
+    @SuppressWarnings("checkstyle:DesignForExtension")
+    public void addResourceHandlers(@NotNull final ResourceHandlerRegistry registry) {
+        // Cache Images
+        registry.addResourceHandler("/images/favicon/**")
+                .addResourceLocations("classpath:/static/images/favicon/")
+                .setCacheControl(DEFAULT_CACHE_DURATION);
+
+        registry.addResourceHandler("/images/logo/**")
+                .addResourceLocations("classpath:/static/images/logo/")
+                .setCacheControl(DEFAULT_CACHE_DURATION);
+
+        registry.addResourceHandler("/images/open_graph/**")
+                .addResourceLocations("classpath:/static/images/open_graph/")
+                .setCacheControl(DEFAULT_CACHE_DURATION);
+
+        // Cache SVG
+        registry.addResourceHandler("/svg/footer/**")
+                .addResourceLocations("classpath:/static/svg/footer/")
+                .setCacheControl(DEFAULT_CACHE_DURATION);
+
+        registry.addResourceHandler("/svg/type_*.svg")
+                .addResourceLocations("classpath:/static/svg/")
+                .setCacheControl(DEFAULT_CACHE_DURATION);
     }
 
 }
