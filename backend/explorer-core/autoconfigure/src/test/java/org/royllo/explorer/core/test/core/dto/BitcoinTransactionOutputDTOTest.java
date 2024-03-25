27503b66c4c5d14c64fec0ae43c7693be4807988
straumat <stephane.traumat@gmail.com>
2024-03-25 09:26:53 +0100

    DTO tests validation

--- backend/explorer-core/autoconfigure/src/test/java/org/royllo/explorer/core/test/core/dto/BitcoinTransactionOutputDTOTest.java
+++ backend/explorer-core/autoconfigure/src/test/java/org/royllo/explorer/core/test/core/dto/BitcoinTransactionOutputDTOTest.java
@@ -6,19 +6,16 @@ import org.royllo.explorer.core.dto.bitcoin.BitcoinTransactionOutputDTO;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayName("BitcoinTransactionOutputDTOTest tests")
+@DisplayName("BitcoinTransactionOutputDTO tests")
 public class BitcoinTransactionOutputDTOTest {
 
     @Test
     @DisplayName("getByteReverseTxId()")
-    public void byteReverseTxId() {
-
+    public void getByteReverseTxId() {
         var transaction = BitcoinTransactionOutputDTO.builder()
                 .txId("6db79f5af2ba65bfb4044ced690f3acb4a791a6fc6a7450664e15559ad770b90")
                 .build();
-
         assertEquals("900b77ad5955e1640645a7c66f1a794acb3a0f69ed4c04b4bf65baf25a9fb76d", transaction.getByteReverseTxId());
-
     }
 
 }
--- backend/explorer-core/autoconfigure/src/test/java/org/royllo/explorer/core/test/core/dto/UserDTOTest.java
+++ backend/explorer-core/autoconfigure/src/test/java/org/royllo/explorer/core/test/core/dto/UserDTOTest.java
@@ -3,42 +3,57 @@ package org.royllo.explorer.core.test.core.dto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.royllo.explorer.core.dto.user.UserDTO;
+import org.royllo.explorer.core.dto.user.UserDTOSettings;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DisplayName("UserDTOTest tests")
+@DisplayName("UserDTO tests")
 public class UserDTOTest {
 
     @Test
     @DisplayName("getShortenedUsername()")
     public void getShortenedUsername() {
         // Null username.
-        UserDTO user = UserDTO.builder()
-                .build();
+        UserDTO user = UserDTO.builder().build();
         assertNull(user.getShortenedUsername());
 
         // Normal username.
-        user = UserDTO.builder()
-                .username("straumat")
-                .build();
+        user = UserDTO.builder().username("straumat").build();
         assertEquals("straumat", user.getShortenedUsername());
 
         // Long username.
-        user = UserDTO.builder()
-                .username("abcedefghijklmnopqrstuvwxyz")
-                .build();
+        user = UserDTO.builder().username("abcedefghijklmnopqrstuvwxyz").build();
         assertEquals("abc...xyz", user.getShortenedUsername());
+    }
+
+    @Test
+    @DisplayName("getCurrentSettings()")
+    public void getCurrentSettings() {
+        UserDTO user = UserDTO.builder()
+                .username("usernameValue")
+                .profilePictureFileName("profilePictureFileNameValue")
+                .fullName("fullNameValue")
+                .biography("biographyValue")
+                .website("websiteValue")
+                .build();
+        UserDTOSettings settings = user.getCurrentSettings();
+        assertNotNull(settings);
+        assertEquals("usernameValue", settings.username());
+        assertEquals("profilePictureFileNameValue", settings.profilePictureFileName());
+        assertEquals("fullNameValue", settings.fullName());
+        assertEquals("biographyValue", settings.biography());
+        assertEquals("websiteValue", settings.website());
     }
 
     @Test
     @DisplayName("hasSettingsSet()")
     public void hasSettingsSet() {
         // No settings set.
-        UserDTO user = UserDTO.builder()
-                .build();
+        UserDTO user = UserDTO.builder().build();
         assertFalse(user.hasSettingsSet());
 
         // Full name set.
